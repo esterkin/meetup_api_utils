7c2536392060f501d8e29220cb9d57803356109b
Edward Sterkin <edwardsterkin@gmail.com>
2016-05-07 01:47:00 -0700

    recursively get the next page of data

--- eventsByPopularity.js
+++ eventsByPopularity.js
@@ -2,28 +2,52 @@ var _ = require('underscore');
 var request = require('request');
 
 var query = {
-	"key":'YOUR MEETUP API KEY',
-	"radius":10,
-	"zip":94121,
-	"time":",1w" // The time range is unbounded on the left but status defaults to "upcoming"
+    "key": 'YOUR MEETUP API KEY',
+    "radius": 25,
+    "zip": 94111,
+    "time": ',2d', // The time range is unbounded on the left but status defaults to "upcoming"
 }
 
-request({url:'https://api.meetup.com/2/open_events', qs:query}, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        
-         var results = JSON.parse(body).results;
+url = 'https://api.meetup.com/2/open_events';
 
-         var projectFields = _.map(results, function(evt){
-         	 return {
-         	 	name: evt.name, 
-         	 	group: evt.group.name,
-         	 	date: new Date(evt.time),
-         	 	url: evt.event_url,
-         	 	rsvp:evt.yes_rsvp_count
+var projectFields = [];
+var requestParams = {
+    url: url,
+    qs: query
+}
 
-			 };
-         });
+function getEventData() {
 
-         console.log(_.sortBy(projectFields,'rsvp').reverse());
-    }
-});
+    request(requestParams, function(error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+            var jsonBody = JSON.parse(body);
+            var meta = jsonBody.meta;
+            var results = jsonBody.results;
+
+            projectFields = projectFields.concat(_.map(results, function(evt) {
+
+                return {
+                    name: evt.name,
+                    group: evt.group.name,
+                    date: new Date(evt.time),
+                    url: evt.event_url,
+                    rsvp: evt.yes_rsvp_count
+
+                };
+            }));
+
+            if (meta.next != "") {
+                // recursively get the next page of data
+                requestParams = meta.next;
+                getEventData();
+            } else {
+                console.log(_.sortBy(projectFields, 'rsvp').reverse());
+            }
+
+        }
+    });
+}
+
+getEventData();
